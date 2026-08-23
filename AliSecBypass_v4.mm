dac127dcf7df75afe2a56df381292510be5bff50
qwert3377 <511620397@qq.com>
2026-08-23 23:30:48 +0800

    更新 AliSecBypass_v4.mm

--- AliSecBypass_v4.mm
+++ AliSecBypass_v4.mm
@@ -875,3 +875,4 @@ static void gh_init(void) {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)),
                    dispatch_get_main_queue(), ^{ gh_addFloatingView(); });
 }
+1
