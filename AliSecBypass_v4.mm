7faf5e9f0b8a3689660dc2089fee9a20ef4965b7
qwert3377 <511620397@qq.com>
2026-08-26 09:23:40 +0800

    更新 AliSecBypass_v4.mm

--- AliSecBypass_v4.mm
+++ AliSecBypass_v4.mm
@@ -1,8 +1,8 @@
 //
-//  ElyndorTV VIP Safe Tweak v11.0
+//  ElyndorTV VIP Delayed Tweak v12.0
 //  Target: LysenthoTVSpace (com.influx4.motion.axis26)
 //  Build: 2026-08-26
-//  Strategy: NSUserDefaults ONLY + file logging + crash-proof
+//  Strategy: Delayed hook (5s after launch) + /tmp logging
 //
 
 #import <Foundation/Foundation.h>
@@ -11,43 +11,28 @@
 #define MAX_TIER 8
 #define VIP_KEY @"kvipstatusstoragekey"
 
-static NSString *gLogPath = nil;
-
-#pragma mark - File Logger (write to App Documents)
+#pragma mark - Ultra-safe logger to /tmp
 
 static void vipLog(NSString *fmt, ...) {
     @try {
-        if (!gLogPath) {
-            NSArray *docs = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-            if (docs.count > 0) {
-                gLogPath = [[docs[0] stringByAppendingPathComponent:@"vip_tweak.log"] copy];
-            }
-        }
-        if (!gLogPath) return;
-
         va_list args;
         va_start(args, fmt);
         NSString *msg = [[NSString alloc] initWithFormat:fmt arguments:args];
         va_end(args);
 
-        NSString *line = [NSString stringWithFormat:@"[%@] %@\n",
+        NSString *line = [NSString stringWithFormat:@"%@ [VIP] %@\n",
             [[NSDate date] descriptionWithLocale:nil], msg];
 
         NSData *data = [line dataUsingEncoding:NSUTF8StringEncoding];
-        NSFileManager *fm = [NSFileManager defaultManager];
-        if (![fm fileExistsAtPath:gLogPath]) {
-            [fm createFileAtPath:gLogPath contents:data attributes:nil];
+        NSFileHandle *fh = [NSFileHandle fileHandleForWritingAtPath:@"/tmp/vip_tweak.log"];
+        if (fh) {
+            [fh seekToEndOfFile];
+            [fh writeData:data];
+            [fh closeFile];
         } else {
-            NSFileHandle *fh = [NSFileHandle fileHandleForWritingAtPath:gLogPath];
-            if (fh) {
-                [fh seekToEndOfFile];
-                [fh writeData:data];
-                [fh closeFile];
-            }
+            [data writeToFile:@"/tmp/vip_tweak.log" atomically:YES];
         }
-    } @catch (NSException *e) {
-        // silent fail
-    }
+    } @catch (NSException *e) {}
 }
 
 #pragma mark - Fake Data
@@ -91,21 +76,13 @@ static id  (*orig_objectForKey)(id, SEL, NSString *);
 static id  (*orig_stringForKey)(id, SEL, NSString *);
 static id  (*orig_dataForKey)(id, SEL, NSString *);
 static id  (*orig_dictionaryForKey)(id, SEL, NSString *);
-static id  (*orig_arrayForKey)(id, SEL, NSString *);
 static NSInteger (*orig_integerForKey)(id, SEL, NSString *);
-static int   (*orig_intForKey)(id, SEL, NSString *);
 static BOOL  (*orig_boolForKey)(id, SEL, NSString *);
-static double (*orig_doubleForKey)(id, SEL, NSString *);
-static float (*orig_floatForKey)(id, SEL, NSString *);
 
 static void (*orig_setObject)(id, SEL, id, NSString *);
-static void (*orig_setInteger)(id, SEL, NSInteger, NSString *);
-static void (*orig_setBool)(id, SEL, BOOL, NSString *);
-static void (*orig_setDouble)(id, SEL, double, NSString *);
-static void (*orig_setFloat)(id, SEL, float, NSString *);
 static void (*orig_removeObject)(id, SEL, NSString *);
 
-#pragma mark - Hooks
+#pragma mark - Minimal Hooks (only 5 most critical)
 
 static id hook_objectForKey(id self, SEL _cmd, NSString *key) {
     @try {
@@ -147,13 +124,6 @@ static id hook_dictionaryForKey(id self, SEL _cmd, NSString *key) {
     return orig_dictionaryForKey(self, _cmd, key);
 }
 
-static id hook_arrayForKey(id self, SEL _cmd, NSString *key) {
-    @try {
-        if (isMemberKey(key)) return @[];
-    } @catch (NSException *e) {}
-    return orig_arrayForKey(self, _cmd, key);
-}
-
 static NSInteger hook_integerForKey(id self, SEL _cmd, NSString *key) {
     @try {
         if (isMemberKey(key)) {
@@ -162,13 +132,6 @@ static NSInteger hook_integerForKey(id self, SEL _cmd, NSString *key) {
         }
     } @catch (NSException *e) {}
     return orig_integerForKey(self, _cmd, key);
-}
-
-static int hook_intForKey(id self, SEL _cmd, NSString *key) {
-    @try {
-        if (isMemberKey(key)) return MAX_TIER;
-    } @catch (NSException *e) {}
-    return orig_intForKey(self, _cmd, key);
 }
 
 static BOOL hook_boolForKey(id self, SEL _cmd, NSString *key) {
@@ -181,20 +144,6 @@ static BOOL hook_boolForKey(id self, SEL _cmd, NSString *key) {
     return orig_boolForKey(self, _cmd, key);
 }
 
-static double hook_doubleForKey(id self, SEL _cmd, NSString *key) {
-    @try {
-        if (isMemberKey(key)) return 4102444800.0;
-    } @catch (NSException *e) {}
-    return orig_doubleForKey(self, _cmd, key);
-}
-
-static float hook_floatForKey(id self, SEL _cmd, NSString *key) {
-    @try {
-        if (isMemberKey(key)) return 4102444800.0f;
-    } @catch (NSException *e) {}
-    return orig_floatForKey(self, _cmd, key);
-}
-
 static void hook_setObject(id self, SEL _cmd, id value, NSString *key) {
     @try {
         if (isMemberKey(key)) {
@@ -203,46 +152,6 @@ static void hook_setObject(id self, SEL _cmd, id value, NSString *key) {
         }
     } @catch (NSException *e) {}
     orig_setObject(self, _cmd, value, key);
-}
-
-static void hook_setInteger(id self, SEL _cmd, NSInteger value, NSString *key) {
-    @try {
-        if (isMemberKey(key)) {
-            vipLog(@"[BLOCK] setInteger:forKey:'%@'", key);
-            return;
-        }
-    } @catch (NSException *e) {}
-    orig_setInteger(self, _cmd, value, key);
-}
-
-static void hook_setBool(id self, SEL _cmd, BOOL value, NSString *key) {
-    @try {
-        if (isMemberKey(key)) {
-            vipLog(@"[BLOCK] setBool:forKey:'%@'", key);
-            return;
-        }
-    } @catch (NSException *e) {}
-    orig_setBool(self, _cmd, value, key);
-}
-
-static void hook_setDouble(id self, SEL _cmd, double value, NSString *key) {
-    @try {
-        if (isMemberKey(key)) {
-            vipLog(@"[BLOCK] setDouble:forKey:'%@'", key);
-            return;
-        }
-    } @catch (NSException *e) {}
-    orig_setDouble(self, _cmd, value, key);
-}
-
-static void hook_setFloat(id self, SEL _cmd, float value, NSString *key) {
-    @try {
-        if (isMemberKey(key)) {
-            vipLog(@"[BLOCK] setFloat:forKey:'%@'", key);
-            return;
-        }
-    } @catch (NSException *e) {}
-    orig_setFloat(self, _cmd, value, key);
 }
 
 static void hook_removeObject(id self, SEL _cmd, NSString *key) {
@@ -255,79 +164,80 @@ static void hook_removeObject(id self, SEL _cmd, NSString *key) {
     orig_removeObject(self, _cmd, key);
 }
 
-#pragma mark - Swizzle Helper
+#pragma mark - Swizzle
 
 static void swizzle(Class cls, SEL sel, IMP newImp, IMP *origImp) {
     @try {
         Method m = class_getInstanceMethod(cls, sel);
-        if (!m) m = class_getClassMethod(cls, sel);
         if (!m) {
             vipLog(@"[WARN] method not found %@ %@", NSStringFromClass(cls), NSStringFromSelector(sel));
             return;
         }
         *origImp = method_setImplementation(m, newImp);
+        vipLog(@"[OK] Swizzled %@ %@", NSStringFromClass(cls), NSStringFromSelector(sel));
     } @catch (NSException *e) {
         vipLog(@"[WARN] swizzle exception: %@", e);
     }
 }
 
-#pragma mark - Constructor
+#pragma mark - Core Logic (called after delay)
 
-__attribute__((constructor))
-static void init(void) {
-    // Init log path early
-    NSArray *docs = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    if (docs.count > 0) {
-        gLogPath = [[docs[0] stringByAppendingPathComponent:@"vip_tweak.log"] copy];
-    }
-
+static void doHook(void) {
     vipLog(@"========================================");
-    vipLog(@"ElyndorTV VIP Safe Tweak v11.0 Loading");
+    vipLog(@"ElyndorTV VIP Delayed Tweak v12.0");
     vipLog(@"Build: 2026-08-26");
     vipLog(@"Target: LysenthoTVSpace");
     vipLog(@"Max Tier: %d", MAX_TIER);
-    vipLog(@"Log: %@", gLogPath ?: @"<unknown>");
     vipLog(@"========================================");
 
-    Class UD = [NSUserDefaults class];
-    swizzle(UD, @selector(objectForKey:),            (IMP)hook_objectForKey,      (IMP *)&orig_objectForKey);
-    swizzle(UD, @selector(stringForKey:),            (IMP)hook_stringForKey,      (IMP *)&orig_stringForKey);
-    swizzle(UD, @selector(dataForKey:),              (IMP)hook_dataForKey,        (IMP *)&orig_dataForKey);
-    swizzle(UD, @selector(dictionaryForKey:),        (IMP)hook_dictionaryForKey,  (IMP *)&orig_dictionaryForKey);
-    swizzle(UD, @selector(arrayForKey:),             (IMP)hook_arrayForKey,       (IMP *)&orig_arrayForKey);
-    swizzle(UD, @selector(integerForKey:),           (IMP)hook_integerForKey,     (IMP *)&orig_integerForKey);
-    swizzle(UD, @selector(intForKey:),               (IMP)hook_intForKey,         (IMP *)&orig_intForKey);
-    swizzle(UD, @selector(boolForKey:),              (IMP)hook_boolForKey,        (IMP *)&orig_boolForKey);
-    swizzle(UD, @selector(doubleForKey:),            (IMP)hook_doubleForKey,      (IMP *)&orig_doubleForKey);
-    swizzle(UD, @selector(floatForKey:),             (IMP)hook_floatForKey,       (IMP *)&orig_floatForKey);
-    swizzle(UD, @selector(setObject:forKey:),        (IMP)hook_setObject,         (IMP *)&orig_setObject);
-    swizzle(UD, @selector(setInteger:forKey:),       (IMP)hook_setInteger,        (IMP *)&orig_setInteger);
-    swizzle(UD, @selector(setBool:forKey:),          (IMP)hook_setBool,           (IMP *)&orig_setBool);
-    swizzle(UD, @selector(setDouble:forKey:),        (IMP)hook_setDouble,         (IMP *)&orig_setDouble);
-    swizzle(UD, @selector(setFloat:forKey:),         (IMP)hook_setFloat,          (IMP *)&orig_setFloat);
-    swizzle(UD, @selector(removeObjectForKey:),      (IMP)hook_removeObject,      (IMP *)&orig_removeObject);
+    // Check if NSUserDefaults class is loaded
+    Class UD = objc_getClass("NSUserDefaults");
+    if (!UD) {
+        vipLog(@"[ERR] NSUserDefaults class not found, aborting");
+        return;
+    }
+    vipLog(@"[OK] NSUserDefaults class found");
 
-    // Initial injection
+    // Swizzle only the most critical methods
+    swizzle(UD, @selector(objectForKey:),     (IMP)hook_objectForKey,     (IMP *)&orig_objectForKey);
+    swizzle(UD, @selector(stringForKey:),     (IMP)hook_stringForKey,     (IMP *)&orig_stringForKey);
+    swizzle(UD, @selector(dataForKey:),       (IMP)hook_dataForKey,       (IMP *)&orig_dataForKey);
+    swizzle(UD, @selector(dictionaryForKey:), (IMP)hook_dictionaryForKey, (IMP *)&orig_dictionaryForKey);
+    swizzle(UD, @selector(integerForKey:),    (IMP)hook_integerForKey,    (IMP *)&orig_integerForKey);
+    swizzle(UD, @selector(boolForKey:),       (IMP)hook_boolForKey,       (IMP *)&orig_boolForKey);
+    swizzle(UD, @selector(setObject:forKey:), (IMP)hook_setObject,        (IMP *)&orig_setObject);
+    swizzle(UD, @selector(removeObjectForKey:), (IMP)hook_removeObject,   (IMP *)&orig_removeObject);
+
+    // Inject into existing standardUserDefaults
     @try {
         NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
-        [ud setObject:buildFakeVipDict() forKey:VIP_KEY];
-        [ud synchronize];
-        vipLog(@"[INIT] Injected fake VIP dict at load time");
+        if (ud) {
+            [ud setObject:buildFakeVipDict() forKey:VIP_KEY];
+            [ud synchronize];
+            vipLog(@"[INIT] Injected fake VIP dict");
+        } else {
+            vipLog(@"[INIT] standardUserDefaults is nil");
+        }
     } @catch (NSException *e) {
-        vipLog(@"[INIT] Injection failed: %@", e);
+        vipLog(@"[INIT] Injection exception: %@", e);
     }
 
-    // Re-inject after delay
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        @try {
-            NSUserDefaults *ud2 = [NSUserDefaults standardUserDefaults];
-            [ud2 setObject:buildFakeVipDict() forKey:VIP_KEY];
-            [ud2 synchronize];
-            vipLog(@"[INIT] Re-injected after 3s delay");
-        } @catch (NSException *e) {
-            vipLog(@"[INIT] Re-inject failed: %@", e);
-        }
-    });
+    vipLog(@"[INIT] All hooks installed");
+}
 
-    vipLog(@"[INIT] All hooks installed, tweak active");
+#pragma mark - Constructor (DO NOTHING except schedule delay)
+
+__attribute__((constructor))
+static void init(void) {
+    // Write a startup marker immediately
+    @try {
+        NSString *marker = @"[STARTUP] Constructor called\n";
+        NSData *data = [marker dataUsingEncoding:NSUTF8StringEncoding];
+        [data writeToFile:@"/tmp/vip_tweak.log" atomically:YES];
+    } @catch (NSException *e) {}
+
+    // Schedule actual work after 5 seconds
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        doHook();
+    });
 }
