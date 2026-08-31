7418cae94301b5319eea24c306c48f3af36f59e2
qwert3377 <511620397@qq.com>
2026-08-31 22:49:23 +0800

    更新 AliSecBypass_v4.mm

--- AliSecBypass_v4.mm
+++ AliSecBypass_v4.mm
@@ -1,189 +1,247 @@
-//
-//  kiosker_premium_network.mm
-//  Kiosker Premium Unlock - Network Layer Interception
-//  Target: com.c-konsult.kiosker-sub
-//
+// kiosker.mm - Kiosker TrollStore Plugin
+// Theos .mm file for TrollStore injection
+// Compile with Theos: make package
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
+#import <substrate.h>
 
-#pragma mark - Logging
+#define STATE_SUBSCRIBED 2
 
-static void klog(NSString *msg) {
-    @autoreleasepool {
-        NSString *doc = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-        if (!doc) return;
-        NSString *path = [doc stringByAppendingPathComponent:@"kiosker_premium.log"];
-        NSString *line = [NSString stringWithFormat:@"[%@] %@\n", [NSDate date], msg];
-        NSFileManager *fm = [NSFileManager defaultManager];
-        if (![fm fileExistsAtPath:path]) {
-            [line writeToFile:path atomically:YES encoding:NSUTF8StringEncoding error:nil];
-        } else {
-            NSFileHandle *fh = [NSFileHandle fileHandleForWritingAtPath:path];
-            if (fh) {
-                [fh seekToEndOfFile];
-                [fh writeData:[line dataUsingEncoding:NSUTF8StringEncoding]];
-                [fh closeFile];
-            }
+static void forceState(id inst) {
+    if (!inst) return;
+    void *ptr = (__bridge void *)inst;
+    if (!ptr) return;
+    @try {
+        uint8_t *statePtr = (uint8_t *)ptr + 32;
+        *statePtr = STATE_SUBSCRIBED;
+    } @catch (NSException *e) {}
+}
+
+static uint8_t readState(id inst) {
+    if (!inst) return 255;
+    void *ptr = (__bridge void *)inst;
+    if (!ptr) return 255;
+    @try {
+        uint8_t *statePtr = (uint8_t *)ptr + 32;
+        return *statePtr;
+    } @catch (NSException *e) { return 255; }
+}
+
+static void triggerTimerTick(id inst) {
+    if (!inst) return;
+    Class cls = NSClassFromString(@"Kiosker.SubscriptionHandler");
+    if (!cls) return;
+    SEL sel = NSSelectorFromString(@"timerTick");
+    if (!sel) return;
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+    @try {
+        [inst performSelector:sel];
+    } @catch (NSException *e) {}
+    #pragma clang diagnostic pop
+}
+
+// ========== UISwitch helpers ==========
+static void enableSwitch(UISwitch *sw) {
+    if (!sw) return;
+    @try {
+        [sw setEnabled:YES];
+        if (![sw isOn]) {
+            [sw setOn:YES animated:NO];
         }
+    } @catch (NSException *e) {}
+}
+
+static void scanViewForSwitches(UIView *view, int depth) {
+    if (!view || depth > 30) return;
+    if ([view isKindOfClass:[UISwitch class]]) {
+        enableSwitch((UISwitch *)view);
+    }
+    NSArray *subs = [view subviews];
+    for (UIView *sub in subs) {
+        scanViewForSwitches(sub, depth + 1);
     }
 }
 
-#pragma mark - Fake RevenueCat Data
-
-static NSData *fakeRCData(void) {
-    NSDictionary *premiumEntitlement = @{
-        @"expires_date": @"2099-12-31T23:59:59Z",
-        @"product_identifier": @"com.c-konsult.kiosker-sub.premium",
-        @"purchase_date": @"2026-08-01T00:00:00Z"
-    };
-
-    NSDictionary *entitlements = @{
-        @"premium": premiumEntitlement
-    };
-
-    NSDictionary *subscription = @{
-        @"billing_issues_detected_at": [NSNull null],
-        @"expires_date": @"2099-12-31T23:59:59Z",
-        @"grace_period_expires_date": [NSNull null],
-        @"is_sandbox": @NO,
-        @"original_purchase_date": @"2026-08-01T00:00:00Z",
-        @"period_type": @"normal",
-        @"purchase_date": @"2026-08-01T00:00:00Z",
-        @"store": @"app_store",
-        @"unsubscribe_detected_at": [NSNull null]
-    };
-
-    NSDictionary *subscriber = @{
-        @"entitlements": entitlements,
-        @"first_seen": @"2026-08-01T00:00:00Z",
-        @"original_app_user_id": @"kiosker_user",
-        @"original_application_version": @"281",
-        @"other_purchases": @{},
-        @"subscriptions": @{
-            @"com.c-konsult.kiosker-sub.premium": subscription
+static void scanAllWindows() {
+    @try {
+        UIApplication *app = [UIApplication sharedApplication];
+        if (!app) return;
+        NSArray *windows = [app windows];
+        for (UIWindow *win in windows) {
+            scanViewForSwitches(win, 0);
         }
-    };
-
-    NSDictionary *root = @{
-        @"request_date": @"2026-08-31T00:00:00Z",
-        @"request_date_ms": @1756588800000,
-        @"subscriber": subscriber
-    };
-
-    NSError *err = nil;
-    NSData *data = [NSJSONSerialization dataWithJSONObject:root options:0 error:&err];
-    if (err) {
-        klog([NSString stringWithFormat:@"[ERR] JSON serialize error: %@", err]);
-    }
-    return data;
+    } @catch (NSException *e) {}
 }
 
-#pragma mark - Original IMPs
-
-static IMP orig_dataTask = NULL;
-static IMP orig_presentVC = NULL;
-static IMP orig_addPayment = NULL;
-
-#pragma mark - Hooked Methods
-
-// Hook NSURLSession -dataTaskWithRequest:completionHandler:
-static NSURLSessionDataTask *hooked_dataTask(id self, SEL _cmd, NSURLRequest *request, void (^completionHandler)(NSData*, NSURLResponse*, NSError*)) {
-    NSString *urlStr = request.URL.absoluteString;
-
-    // Detect RevenueCat API calls
-    if ([urlStr containsString:@"api.revenuecat.com"] ||
-        [urlStr containsString:@"revenuecat"] ||
-        [urlStr containsString:@"purchases.revenuecat"]) {
-
-        klog([NSString stringWithFormat:@"[NET] Intercepted RevenueCat API: %@", urlStr]);
-
-        // Return fake data immediately
-        NSData *fakeData = fakeRCData();
-        NSHTTPURLResponse *fakeResponse = [[NSHTTPURLResponse alloc] initWithURL:request.URL
-                                                                        statusCode:200
-                                                                       HTTPVersion:@"HTTP/1.1"
-                                                                      headerFields:@{@"Content-Type": @"application/json"}];
-
-        if (completionHandler) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                completionHandler(fakeData, fakeResponse, nil);
-            });
-        }
-
-        // Return a dummy task
-        return [[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:@"about:blank"]];
-    }
-
-    if (orig_dataTask) {
-        return ((NSURLSessionDataTask *(*)(id, SEL, NSURLRequest*, void (^)(NSData*, NSURLResponse*, NSError*)))orig_dataTask)(self, _cmd, request, completionHandler);
-    }
-    return nil;
-}
-
-// Hook UIViewController -presentViewController:animated:completion:
-static void hooked_presentVC(id self, SEL _cmd, UIViewController *vc, BOOL animated, id completion) {
-    NSString *clsName = NSStringFromClass([vc class]);
-    if ([clsName containsString:@"PresentationHostingController"] ||
-        [clsName containsString:@"UIHostingController"] ||
-        [clsName containsString:@"SKStore"] ||
-        [clsName containsString:@"Paywall"]) {
-        klog([NSString stringWithFormat:@"[POPUP] Blocked: %@", clsName]);
-        return;
-    }
-    if (orig_presentVC) {
-        ((void (*)(id, SEL, UIViewController*, BOOL, id))orig_presentVC)(self, _cmd, vc, animated, completion);
-    }
-}
-
-// Hook SKPaymentQueue -addPayment:
-static void hooked_addPayment(id self, SEL _cmd, id payment) {
-    klog(@"[IAP] Blocked addPayment");
-}
-
-#pragma mark - Installation
-
-static void install_hooks(void) {
-    klog(@"=== Kiosker Premium Network v3.0 ===");
-
-    // 1. Hook NSURLSession dataTaskWithRequest:completionHandler:
-    Class sessClass = [NSURLSession class];
-    Method mTask = class_getInstanceMethod(sessClass, @selector(dataTaskWithRequest:completionHandler:));
-    if (mTask) {
-        orig_dataTask = method_setImplementation(mTask, (IMP)hooked_dataTask);
-        klog(@"[HOOK] NSURLSession dataTaskWithRequest: hooked");
+// ========== Hook helpers ==========
+static void hookClassMethod(const char *clsName, const char *selName, IMP newImp, IMP *origImp) {
+    Class cls = objc_getClass(clsName);
+    if (!cls) return;
+    SEL sel = sel_getUid(selName);
+    if (!sel) return;
+    Method m = class_getInstanceMethod(cls, sel);
+    if (!m) return;
+    if (origImp) {
+        *origImp = method_getImplementation(m);
+        method_setImplementation(m, newImp);
     } else {
-        klog(@"[ERR] NSURLSession dataTaskWithRequest: not found");
+        MSHookMessageEx(cls, sel, newImp, NULL);
+    }
+}
+
+// ========== SubscriptionHandler hooks ==========
+static IMP orig_SH_init = NULL;
+id hook_SH_init(id self, SEL _cmd) {
+    id result = ((id (*)(id, SEL))orig_SH_init)(self, _cmd);
+    forceState(result);
+    return result;
+}
+
+static IMP orig_SH_timerTick = NULL;
+void hook_SH_timerTick(id self, SEL _cmd) {
+    forceState(self);
+    ((void (*)(id, SEL))orig_SH_timerTick)(self, _cmd);
+    // onLeave: check again
+    if (readState(self) != STATE_SUBSCRIBED) {
+        forceState(self);
+    }
+}
+
+static IMP orig_SH_rcCallback = NULL;
+void hook_SH_rcCallback(id self, SEL _cmd, id purchases, id customerInfo) {
+    forceState(self);
+    ((void (*)(id, SEL, id, id))orig_SH_rcCallback)(self, _cmd, purchases, customerInfo);
+    forceState(self);
+    triggerTimerTick(self);
+}
+
+// ========== RevenueCat hooks ==========
+static IMP orig_RC_entitlements = NULL;
+id hook_RC_entitlements(id self, SEL _cmd) {
+    NSMutableDictionary *fake = [NSMutableDictionary dictionary];
+    [fake setObject:@"pro" forKey:@"premium"];
+    return fake;
+}
+
+static IMP orig_RC_activeSubscriptions = NULL;
+id hook_RC_activeSubscriptions(id self, SEL _cmd) {
+    return [NSSet setWithObject:@"premium_monthly"];
+}
+
+static IMP orig_RCEntitlement_isActive = NULL;
+BOOL hook_RCEntitlement_isActive(id self, SEL _cmd) {
+    return YES;
+}
+
+// ========== UIViewController viewDidAppear ==========
+static IMP orig_VC_viewDidAppear = NULL;
+void hook_VC_viewDidAppear(id self, SEL _cmd, BOOL animated) {
+    ((void (*)(id, SEL, BOOL))orig_VC_viewDidAppear)(self, _cmd, animated);
+
+    NSString *clsName = NSStringFromClass([self class]);
+    if ([clsName rangeOfString:@"HostingController"].location != NSNotFound ||
+        [clsName rangeOfString:@"HostingView"].location != NSNotFound) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            scanAllWindows();
+            // Also fix SubscriptionHandler state
+            Class shClass = NSClassFromString(@"Kiosker.SubscriptionHandler");
+            if (shClass) {
+                unsigned int count = 0;
+                id *instances = (id *)objc_copyClassInstances((__bridge Class _Nonnull)(shClass), &count);
+                // Note: objc_copyClassInstances is not public API, use alternative
+            }
+        });
+    }
+}
+
+// ========== UIControl sendAction ==========
+static IMP orig_Control_sendAction = NULL;
+void hook_Control_sendAction(id self, SEL _cmd, SEL action, id target, UIEvent *event) {
+    ((void (*)(id, SEL, SEL, id, UIEvent *))orig_Control_sendAction)(self, _cmd, action, target, event);
+    if ([self isKindOfClass:[UISwitch class]]) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.05 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            enableSwitch((UISwitch *)self);
+        });
+    }
+}
+
+// ========== Constructor ==========
+__attribute__((constructor))
+static void kioskerInit() {
+    NSLog(@"[KIOSKER] Plugin loading...");
+
+    // Hook SubscriptionHandler
+    Class shClass = NSClassFromString(@"Kiosker.SubscriptionHandler");
+    if (shClass) {
+        Method mInit = class_getInstanceMethod(shClass, @selector(init));
+        if (mInit) {
+            orig_SH_init = method_getImplementation(mInit);
+            method_setImplementation(mInit, (IMP)hook_SH_init);
+        }
+
+        Method mTick = class_getInstanceMethod(shClass, @selector(timerTick));
+        if (mTick) {
+            orig_SH_timerTick = method_getImplementation(mTick);
+            method_setImplementation(mTick, (IMP)hook_SH_timerTick);
+        }
+
+        Method mRC = class_getInstanceMethod(shClass, @selector(purchases:receivedUpdatedCustomerInfo:));
+        if (mRC) {
+            orig_SH_rcCallback = method_getImplementation(mRC);
+            method_setImplementation(mRC, (IMP)hook_SH_rcCallback);
+        }
+
+        // Fix existing instance
+        @try {
+            id existing = nil;
+            // Try to find existing instance via notification center or shared instance
+            // If not available, timerTick will catch it
+        } @catch (NSException *e) {}
     }
 
-    // 2. Hook UIViewController presentViewController
-    Class vcClass = [UIViewController class];
-    Method mPresent = class_getInstanceMethod(vcClass, @selector(presentViewController:animated:completion:));
-    if (mPresent) {
-        orig_presentVC = method_setImplementation(mPresent, (IMP)hooked_presentVC);
-        klog(@"[HOOK] UIViewController presentViewController: hooked");
-    }
+    // Hook RevenueCat
+    Class rcClass = NSClassFromString(@"RCCustomerInfo");
+    if (rcClass) {
+        Method mEnt = class_getInstanceMethod(rcClass, @selector(entitlements));
+        if (mEnt) {
+            orig_RC_entitlements = method_getImplementation(mEnt);
+            method_setImplementation(mEnt, (IMP)hook_RC_entitlements);
+        }
 
-    // 3. Hook SKPaymentQueue
-    Class skClass = NSClassFromString(@"SKPaymentQueue");
-    if (skClass) {
-        Method mAdd = class_getInstanceMethod(skClass, @selector(addPayment:));
-        if (mAdd) {
-            orig_addPayment = method_setImplementation(mAdd, (IMP)hooked_addPayment);
-            klog(@"[HOOK] SKPaymentQueue addPayment: hooked");
+        Method mSub = class_getInstanceMethod(rcClass, @selector(activeSubscriptions));
+        if (mSub) {
+            orig_RC_activeSubscriptions = method_getImplementation(mSub);
+            method_setImplementation(mSub, (IMP)hook_RC_activeSubscriptions);
         }
     }
 
-    klog(@"[DONE] All hooks installed.");
-}
+    Class rcEntClass = NSClassFromString(@"RCEntitlementInfo");
+    if (rcEntClass) {
+        Method mActive = class_getInstanceMethod(rcEntClass, @selector(isActive));
+        if (mActive) {
+            orig_RCEntitlement_isActive = method_getImplementation(mActive);
+            method_setImplementation(mActive, (IMP)hook_RCEntitlement_isActive);
+        }
+    }
 
-#pragma mark - Constructor
+    // Hook UIViewController
+    Class vcClass = [UIViewController class];
+    Method mAppear = class_getInstanceMethod(vcClass, @selector(viewDidAppear:));
+    if (mAppear) {
+        orig_VC_viewDidAppear = method_getImplementation(mAppear);
+        method_setImplementation(mAppear, (IMP)hook_VC_viewDidAppear);
+    }
 
-__attribute__((constructor))
-static void constructor(void) {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)),
-                   dispatch_get_main_queue(), ^{
-        install_hooks();
-    });
+    // Hook UIControl
+    Class ctrlClass = [UIControl class];
+    Method mSend = class_getInstanceMethod(ctrlClass, @selector(sendAction:to:forEvent:));
+    if (mSend) {
+        orig_Control_sendAction = method_getImplementation(mSend);
+        method_setImplementation(mSend, (IMP)hook_Control_sendAction);
+    }
+
+    NSLog(@"[KIOSKER] Plugin loaded.");
 }
