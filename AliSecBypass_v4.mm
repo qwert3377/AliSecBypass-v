d5e613fb4026324f12f27a7bd74dc9f5c56776ef
qwert3377 <511620397@qq.com>
2026-09-01 16:05:05 +0800

    更新 AliSecBypass_v4.mm

--- AliSecBypass_v4.mm
+++ AliSecBypass_v4.mm
@@ -1,32 +1,28 @@
 //
 //  Kiosker_Premium_Unlock.mm
 //  TrollStore Injection Plugin for Kiosker 26.4.2
-//  Logs to app Documents directory for debugging
+//  Logs to app Documents directory
+//  Blocks purchase popups by intercepting view controller presentation
 //
 
 #import <objc/runtime.h>
 #import <Foundation/Foundation.h>
-#import <sys/stat.h>
+#import <UIKit/UIKit.h>
 
 static id (*orig_init)(id, SEL) = NULL;
 static NSString *logPath = nil;
 
-// Write log to file in app Documents directory
 static void fileLog(NSString *fmt, ...) {
     if (!logPath) {
         NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
         NSString *docDir = [paths firstObject];
         logPath = [docDir stringByAppendingPathComponent:@"kiosker_premium.log"];
     }
-
     va_list args;
     va_start(args, fmt);
     NSString *msg = [[NSString alloc] initWithFormat:fmt arguments:args];
     va_end(args);
-
-    NSString *line = [NSString stringWithFormat:@"[%@] %@\n", 
-                      [NSDate date], msg];
-
+    NSString *line = [NSString stringWithFormat:@"[%@] %@\n", [NSDate date], msg];
     NSFileHandle *fh = [NSFileHandle fileHandleForWritingAtPath:logPath];
     if (!fh) {
         [line writeToFile:logPath atomically:YES encoding:NSUTF8StringEncoding error:nil];
@@ -35,73 +31,161 @@ static void fileLog(NSString *fmt, ...) {
         [fh writeData:[line dataUsingEncoding:NSUTF8StringEncoding]];
         [fh closeFile];
     }
-
-    // Also NSLog for console visibility
     NSLog(@"[KioskerPremium] %@", msg);
 }
 
+// ==================== 1. SubscriptionHandler init patch ====================
+
 static id kiosker_init(id self, SEL _cmd) {
-    fileLog(@"kiosker_init called, self=%p", self);
+    fileLog(@"init called self=%p", self);
     self = orig_init(self, _cmd);
     if (self) {
-        void *ptr = (__bridge void *)self;
-        uint8_t *base = (uint8_t *)ptr;
-        uint8_t oldState = base[24];
+        uint8_t *base = (uint8_t *)(__bridge void *)self;
+        uint8_t old = base[24];
         base[24] = 1;
-        fileLog(@"Patched _state: %d -> 1 at offset +24, self=%p", oldState, self);
-    } else {
-        fileLog(@"init returned nil");
+        fileLog(@"PATCHED _state %d->1 offset=+24", old);
     }
     return self;
 }
 
-static void tryHook() {
-    fileLog(@"tryHook called");
+// ==================== 2. Block purchase popups ====================
 
-    Class cls = NSClassFromString(@"_TtC7Kiosker19SubscriptionHandler");
-    if (!cls) {
-        fileLog(@"SubscriptionHandler class NOT FOUND yet");
-        return;
+// Hook UIViewController presentViewController to block purchase sheets
+static void (*orig_present)(id, SEL, id, BOOL, id) = NULL;
+static void hook_present(id self, SEL _cmd, UIViewController *vc, BOOL animated, id completion) {
+    NSString *className = NSStringFromClass([vc class]);
+    fileLog(@"presentViewController: %@", className);
+
+    // Block known purchase/subscription view controllers
+    NSArray *blocked = @[@"SKStoreProductViewController",
+                         @"RCPaywallViewController",
+                         @"PaywallViewController",
+                         @"SubscriptionViewController",
+                         @"PurchaseViewController",
+                         @"UIAlertController"];
+
+    for (NSString *blockedClass in blocked) {
+        if ([className isEqualToString:blockedClass] || [vc isKindOfClass:NSClassFromString(blockedClass)]) {
+            fileLog(@"BLOCKED popup: %@", className);
+            if (completion) {
+                void (^block)(void) = completion;
+                block();
+            }
+            return;
+        }
     }
-    fileLog(@"Found class: %@", cls);
 
-    Method m = class_getInstanceMethod(cls, @selector(init));
-    if (!m) {
-        fileLog(@"init method NOT FOUND");
-        return;
+    // Also block if title contains purchase keywords
+    if ([vc respondsToSelector:@selector(title)]) {
+        NSString *title = [(UIViewController *)vc title];
+        if (title && ([title rangeOfString:@"订阅"].location != NSNotFound ||
+                      [title rangeOfString:@"Premium"].location != NSNotFound ||
+                      [title rangeOfString:@"购买"].location != NSNotFound ||
+                      [title rangeOfString:@"Upgrade"].location != NSNotFound)) {
+            fileLog(@"BLOCKED popup by title: %@", title);
+            if (completion) {
+                void (^block)(void) = completion;
+                block();
+            }
+            return;
+        }
     }
-    fileLog(@"Found init method");
 
-    IMP current = method_getImplementation(m);
-    if (current == (IMP)kiosker_init) {
-        fileLog(@"Already hooked, skipping");
-        return;
+    orig_present(self, _cmd, vc, animated, completion);
+}
+
+// ==================== 3. Hook RevenueCat purchases ====================
+
+static void (*orig_purchase)(id, SEL, id, id) = NULL;
+static void hook_purchase(id self, SEL _cmd, id product, id completion) {
+    fileLog(@"BLOCKED RevenueCat purchase: %@", product);
+    if (completion) {
+        // Return fake success
+        void (^block)(id, id, id, BOOL) = completion;
+        block(nil, nil, nil, YES);
+    }
+}
+
+// ==================== 4. Hook SKPaymentQueue ====================
+
+static void (*orig_addPayment)(id, SEL, id) = NULL;
+static void hook_addPayment(id self, SEL _cmd, id payment) {
+    fileLog(@"BLOCKED SKPaymentQueue addPayment");
+}
+
+// ==================== Constructor ====================
+
+static void doHook() {
+    // Hook SubscriptionHandler init
+    Class subCls = NSClassFromString(@"_TtC7Kiosker19SubscriptionHandler");
+    if (subCls) {
+        Method m = class_getInstanceMethod(subCls, @selector(init));
+        if (m) {
+            IMP cur = method_getImplementation(m);
+            if (cur != (IMP)kiosker_init) {
+                orig_init = (id (*)(id, SEL))cur;
+                method_setImplementation(m, (IMP)kiosker_init);
+                fileLog(@"HOOKED SubscriptionHandler.init");
+            }
+        }
+    } else {
+        fileLog(@"SubscriptionHandler not found yet");
     }
 
-    orig_init = (id (*)(id, SEL))current;
-    method_setImplementation(m, (IMP)kiosker_init);
-    fileLog(@"HOOKED init: orig=%p, new=%p", orig_init, kiosker_init);
+    // Hook UIViewController presentViewController
+    Class vcCls = [UIViewController class];
+    Method pm = class_getInstanceMethod(vcCls, @selector(presentViewController:animated:completion:));
+    if (pm) {
+        IMP cur = method_getImplementation(pm);
+        if (cur != (IMP)hook_present) {
+            orig_present = (void (*)(id, SEL, id, BOOL, id))cur;
+            method_setImplementation(pm, (IMP)hook_present);
+            fileLog(@"HOOKED UIViewController.presentViewController");
+        }
+    }
+
+    // Hook RCPurchases purchase methods
+    Class rcCls = NSClassFromString(@"RCPurchases");
+    if (rcCls) {
+        Method pm1 = class_getInstanceMethod(rcCls, NSSelectorFromString(@"purchaseProduct:withCompletion:"));
+        if (pm1) {
+            IMP cur = method_getImplementation(pm1);
+            if (cur != (IMP)hook_purchase) {
+                orig_purchase = (void (*)(id, SEL, id, id))cur;
+                method_setImplementation(pm1, (IMP)hook_purchase);
+                fileLog(@"HOOKED RCPurchases.purchaseProduct");
+            }
+        }
+    }
+
+    // Hook SKPaymentQueue addPayment
+    Class skCls = NSClassFromString(@"SKPaymentQueue");
+    if (skCls) {
+        Method am = class_getInstanceMethod(skCls, @selector(addPayment:));
+        if (am) {
+            IMP cur = method_getImplementation(am);
+            if (cur != (IMP)hook_addPayment) {
+                orig_addPayment = (void (*)(id, SEL, id))cur;
+                method_setImplementation(am, (IMP)hook_addPayment);
+                fileLog(@"HOOKED SKPaymentQueue.addPayment");
+            }
+        }
+    }
 }
 
 __attribute__((constructor))
 static void constructor() {
     @autoreleasepool {
-        fileLog(@"=== constructor started ===");
+        fileLog(@"=== constructor ===");
+        doHook();
 
-        // Try immediately
-        tryHook();
-
-        // If class not found, retry every 0.5s for 5 seconds
-        // Swift classes may be loaded lazily
+        // Retry if class not loaded yet
         if (!NSClassFromString(@"_TtC7Kiosker19SubscriptionHandler")) {
-            fileLog(@"Class not found, starting retry loop");
             dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                for (int i = 0; i < 10; i++) {
+                for (int i = 0; i < 20; i++) {
                     [NSThread sleepForTimeInterval:0.5];
-                    tryHook();
-                    if (NSClassFromString(@"_TtC7Kiosker19SubscriptionHandler")) {
-                        break;
-                    }
+                    doHook();
+                    if (NSClassFromString(@"_TtC7Kiosker19SubscriptionHandler")) break;
                 }
             });
         }
