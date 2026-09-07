fdb48cd3a2592088383cabaa9cd5b0c4dc5b7eaa
qwert3377 <511620397@qq.com>
2026-09-07 13:33:02 +0800

    更新 AliSecBypass_v4.mm

--- AliSecBypass_v4.mm
+++ AliSecBypass_v4.mm
@@ -1,120 +1,107 @@
-#import <objc/runtime.h>
-#import <objc/message.h>
+// YSBrowser Pro Unlock - TrollStore Injection (.mm)
+// Environment: Non-jailbreak / TrollStore / Theos
+// Compile as single .mm file, no Logos preprocessing needed.
+
 #import <Foundation/Foundation.h>
-#import <dispatch/dispatch.h>
+#import <dlfcn.h>
+#import <mach-o/dyld.h>
+#import <sys/mman.h>
 
-static NSString *const kTargetURL = @"https://blfsl.com/s/ios108/pgijkyn";
-static volatile BOOL g_sent = NO;
+static uintptr_t g_base = 0;
 
-// ------------------------------------------------------------------
-// Minimal logger (optional, delete if not needed)
-// ------------------------------------------------------------------
-static void ilog(NSString *fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
-    NSString *msg = [[NSString alloc] initWithFormat:fmt arguments:args];
-    va_end(args);
-
-    NSDateFormatter *df = [[NSDateFormatter alloc] init];
-    [df setDateFormat:@"HH:mm:ss.SSS"];
-    NSString *ts = [df stringFromDate:[NSDate date]];
-    NSString *line = [NSString stringWithFormat:@"[%@] %@\n", ts, msg];
-
-    NSData *data = [line dataUsingEncoding:NSUTF8StringEncoding];
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *doc = paths.firstObject ?: NSTemporaryDirectory();
-    NSString *path = [doc stringByAppendingPathComponent:@"inject_log.txt"];
-    NSFileHandle *fh = [NSFileHandle fileHandleForWritingAtPath:path];
-    if (fh) { [fh seekToEndOfFile]; [fh writeData:data]; [fh closeFile]; }
-    else { [data writeToFile:path atomically:YES]; }
-}
-
-// ------------------------------------------------------------------
-// Sync request: blocks caller, max 8s, fires exactly once
-// ------------------------------------------------------------------
-static void sendOnceSync(void) {
-    if (g_sent) return;
-    g_sent = YES;
-
-    @autoreleasepool {
-        NSURL *url = [NSURL URLWithString:kTargetURL];
-        if (!url) return;
-
-        NSMutableURLRequest *req = [NSMutableURLRequest requestWithURL:url
-                                                           cachePolicy:NSURLRequestReloadIgnoringLocalCacheData
-                                                       timeoutInterval:8.0];
-        [req setValue:@"Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1" forHTTPHeaderField:@"User-Agent"];
-        [req setValue:@"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" forHTTPHeaderField:@"Accept"];
-        [req setValue:@"en-US,en;q=0.9" forHTTPHeaderField:@"Accept-Language"];
-        [req setValue:@"gzip, deflate, br" forHTTPHeaderField:@"Accept-Encoding"];
-
-        dispatch_semaphore_t sema = dispatch_semaphore_create(0);
-        NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest:req
-                                                                     completionHandler:^(NSData *d, NSURLResponse *r, NSError *e) {
-            // ilog(@"req done status=%ld", (long)[(NSHTTPURLResponse *)r statusCode]);
-            dispatch_semaphore_signal(sema);
-        }];
-        [task resume];
-        dispatch_semaphore_wait(sema, dispatch_time(DISPATCH_TIME_NOW, 10 * NSEC_PER_SEC));
-    }
-}
-
-// ------------------------------------------------------------------
-// Hook helpers
-// ------------------------------------------------------------------
-typedef void (*v2)(id, SEL);
-typedef void (*v3id)(id, SEL, id);
-typedef BOOL (*b4idid)(id, SEL, id, id);
-
-static v2 orig_run = NULL;
-static v3id orig_setDelegate = NULL;
-
-static void h_run(id self, SEL _cmd) {
-    sendOnceSync();
-    orig_run(self, _cmd);
-}
-
-static void h_setDelegate(id self, SEL _cmd, id delegate) {
-    if (!delegate) { orig_setDelegate(self, _cmd, delegate); return; }
-
-    Class cls = [delegate class];
-    SEL sel = sel_registerName("application:didFinishLaunchingWithOptions:");
-    Method m = class_getInstanceMethod(cls, sel);
-    if (m) {
-        b4idid orig = (b4idid)method_getImplementation(m);
-        method_setImplementation(m, imp_implementationWithBlock(^BOOL(id s, id app, id opt) {
-            sendOnceSync();
-            return orig(s, sel, app, opt);
-        }));
-    } else {
-        SEL sel2 = sel_registerName("applicationDidFinishLaunching:");
-        Method m2 = class_getInstanceMethod(cls, sel2);
-        if (m2) {
-            v3id orig2 = (v3id)method_getImplementation(m2);
-            method_setImplementation(m2, imp_implementationWithBlock(^(id s, id app) {
-                sendOnceSync();
-                orig2(s, sel2, app);
-            }));
+// Find module base address
+static uintptr_t getModuleBase(const char *name) {
+    for (uint32_t i = 0; i < _dyld_image_count(); i++) {
+        const char *imgName = _dyld_get_image_name(i);
+        if (strstr(imgName, name)) {
+            return (uintptr_t)_dyld_get_image_header(i);
         }
     }
-    orig_setDelegate(self, _cmd, delegate);
+    return 0;
 }
 
-// ------------------------------------------------------------------
-// Entry
-// ------------------------------------------------------------------
+// Patch 4 bytes at offset (relative to module base)
+static void patch32(uintptr_t offset, uint32_t value) {
+    uintptr_t addr = g_base + offset;
+    uintptr_t page = addr & ~(PAGE_SIZE - 1);
+    size_t len = PAGE_SIZE;
+
+    // Make page writable
+    mprotect((void *)page, len, PROT_READ | PROT_WRITE | PROT_EXEC);
+
+    // Write
+    *(volatile uint32_t *)addr = value;
+
+    // Restore protection
+    mprotect((void *)page, len, PROT_READ | PROT_EXEC);
+
+    // Clear cache
+    __builtin___clear_cache((char *)addr, (char *)(addr + 4));
+}
+
+static inline void patchNop(uintptr_t offset) { patch32(offset, 0xd503201f); }
+static inline void patchRet(uintptr_t offset) { patch32(offset, 0xd65f03c0); }
+static inline void patchMovW0_1(uintptr_t offset) { patch32(offset, 0x52800020); }
+
+// Replace function body with "mov w0, #1; ret"
+static void patchFuncReturnTrue(uintptr_t offset) {
+    patchMovW0_1(offset);
+    patchRet(offset + 4);
+}
+
+static void setupPatches() {
+    // === Settings page ===
+    // 0x448668: tbz w22, #0, 0x448854 -> nop
+    patchNop(0x448668);
+
+    // === Pro detail page ===
+    // 0x448a84: Pro status check -> return 1
+    patchFuncReturnTrue(0x448a84);
+
+    // 0x448644: tbz w0, #0, 0x448698 -> nop
+    patchNop(0x448644);
+
+    // === Export checks ===
+    // 0x3520f8: tbnz w0, #0, 0x352138 -> b 0x352138
+    // Encoding: b imm26=14 -> 0x1400000e
+    patch32(0x3520f8, 0x1400000e);
+
+    // 0x3509dc: cbz x0, 0x350ab0 -> nop
+    patchNop(0x3509dc);
+
+    // 0x3509f8: cbz x20, 0x350ab0 -> nop
+    patchNop(0x3509f8);
+
+    // 0x35f53c: tbz w21, #0, 0x35f598 -> nop
+    patchNop(0x35f53c);
+
+    // 0x35f50c: Export permission check -> return 1
+    patchFuncReturnTrue(0x35f50c);
+
+    // === Export button Pro paths ===
+    // 0x1829f4: tbz w0, #0, 0x182cf4 -> nop
+    patchNop(0x1829f4);
+
+    // 0x182a50: tbz w0, #0, 0x182cf4 -> nop
+    patchNop(0x182a50);
+
+    // === Pro check ===
+    // 0x40e0d8: tbz w0, #32, 0x40e148 -> nop
+    patchNop(0x40e0d8);
+
+    NSLog(@"[YSB-Pro] All patches applied (base=0x%llx)", (unsigned long long)g_base);
+}
+
 __attribute__((constructor))
-static void init(void) {
-    @autoreleasepool {
-        Class UIApp = objc_getClass("UIApplication");
-        if (!UIApp) return;
-
-        SEL sRun = sel_registerName("run");
-        Method mRun = class_getInstanceMethod(UIApp, sRun);
-        if (mRun) { orig_run = (v2)method_getImplementation(mRun); method_setImplementation(mRun, (IMP)h_run); }
-
-        SEL sDel = sel_registerName("setDelegate:");
-        Method mDel = class_getInstanceMethod(UIApp, sDel);
-        if (mDel) { orig_setDelegate = (v3id)method_getImplementation(mDel); method_setImplementation(mDel, (IMP)h_setDelegate); }
-    }
+static void ysbProInit() {
+    // Delay to ensure YSBrowser module is loaded
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)),
+                   dispatch_get_main_queue(), ^{
+        g_base = getModuleBase("YSBrowser");
+        if (g_base) {
+            setupPatches();
+        } else {
+            NSLog(@"[YSB-Pro] YSBrowser module not found");
+        }
+    });
 }
